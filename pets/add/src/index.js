7d631eedabb2fab9849d94a0c2680cce82b00e43
IVAN NICOLAS ACERO <70918042+ivanAcero@users.noreply.github.com>
2022-06-11 18:14:53 -0500

    :recycle: permisos nuevas tablas dynamoDB

---
+++ pets/add/src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const { sendResponse } = require("../../../response.class");
+
+module.exports.handler = async (event, context, callback) => {
+  console.info("event", event);
+  sendResponse(200, "ok lambda pet add", callback);
+};
